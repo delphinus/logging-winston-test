6c0e517bb9c66034ee659fbfb39b951959b59895
delphinus <me@delphinus.dev>
2019-06-13 15:48:09 +0900

    Simple implementation

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import * as lw from '@google-cloud/logging-winston'
+
+const app = express()
+
+;(async () => {
+  const { mw } = await lw.express.middleware()
+  app.use(mw)
+  app.get('/', (req, res) => {
+    (req as any).log.info('this is log')
+    res.status(200).send('Hello, World!').end()
+  })
+})()
